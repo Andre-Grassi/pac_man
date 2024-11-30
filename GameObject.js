bb8e3991d802007583b895248cba1cd730def901
Andre-Grassi <andregrassi.j@gmail.com>
2024-11-30 01:02:10 -0300

    feat(GameObject): add directions constant

--- GameObject.js
+++ GameObject.js
@@ -12,10 +12,32 @@ class GameObject {
     display.drawRectangle(this.x, this.y, this.width, this.height, this.color);
   }
 
-  move() {
-    this.x += this.speed;
-    this.y += this.speed;
+  move(direction, deltaTime) {
+    switch (direction) {
+      case Direction.UP:
+        this.y -= this.speed * deltaTime;
+        break;
+      case Direction.DOWN:
+        this.y += this.speed * deltaTime;
+        break;
+      case Direction.LEFT:
+        this.x -= this.speed * deltaTime;
+        break;
+      case Direction.RIGHT:
+        this.x += this.speed * deltaTime;
+        break;
+      default:
+        break;
+    }
   }
 }
 
-export default GameObject;
+// Definition of the directions a game object can move
+const Direction = Object.freeze({
+  UP: 0,
+  DOWN: 1,
+  LEFT: 2,
+  RIGHT: 3,
+});
+
+export { GameObject, Direction };
