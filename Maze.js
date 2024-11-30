d39c036c6f9300f7ad33a873fc542259ea8cb349
Andre-Grassi <andregrassi.j@gmail.com>
2024-11-30 13:19:23 -0300

    feat(Maze): define each tile as a game object

--- Maze.js
+++ Maze.js
@@ -1,25 +1,31 @@
+import { GameObject } from './GameObject.js'
+
 class Maze {
   constructor(mazeArray, display) {
     this.mazeArray = mazeArray
-    this.display = display
     this.tileWidth = display.canvas.width / mazeArray[0].length
     this.tileHeight = display.canvas.height / mazeArray.length
+
+    // Array of game objects that represent each tile in the maze
+    this.tiles = []
+
+    // Create game objects for each tile
+    for (let i = 0; i < this.mazeArray.length; i++)
+      for (let j = 0; j < this.mazeArray[i].length; j++)
+        if (this.mazeArray[i][j] === 1)
+          this.tiles.push(
+            new GameObject(
+              this.tileWidth * i,
+              this.tileHeight * j,
+              this.tileWidth,
+              this.tileHeight,
+              'black'
+            )
+          )
   }
 
-  draw() {
-    for (let i = 0; i < this.mazeArray.length; i++) {
-      for (let j = 0; j < this.mazeArray[i].length; j++) {
-        if (this.mazeArray[i][j] === 1) {
-          this.display.drawRectangle(
-            this.tileWidth * i,
-            this.tileHeight * j,
-            this.tileWidth,
-            this.tileHeight,
-            'black'
-          )
-        }
-      }
-    }
+  draw(display) {
+    this.tiles.forEach((object) => object.draw(display))
   }
 }
 
