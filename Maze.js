ad6aeebd750f557595104e1aa2a5049a677a8661
Andre-Grassi <andregrassi.j@gmail.com>
2024-11-30 23:15:57 -0300

    refactor(Maze): rename tileObjects to wallObjects for clarity

--- Maze.js
+++ Maze.js
@@ -6,14 +6,14 @@ class Maze {
     this.tileWidth = display.canvas.width / mazeArray[0].length
     this.tileHeight = display.canvas.height / mazeArray.length
 
-    // Array of game objects that represent each tile in the maze
-    this.tileObjects = []
+    // Array of game objects that represent each wall in the maze
+    this.wallObjects = []
 
-    // Create game objects for each tile
+    // Create game objects for each wall
     for (let i = 0; i < this.mazeArray.length; i++)
       for (let j = 0; j < this.mazeArray[i].length; j++)
         if (this.mazeArray[i][j] === 1)
-          this.tileObjects.push(
+          this.wallObjects.push(
             new GameObject(
               this.tileWidth * j,
               this.tileHeight * i,
@@ -25,7 +25,7 @@ class Maze {
   }
 
   draw(display) {
-    this.tileObjects.forEach((object) => object.draw(display))
+    this.wallObjects.forEach((object) => object.draw(display))
   }
 }
 
