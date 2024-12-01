90e57075a507b82c4e55f9f606e983360b926143
Andre-Grassi <andregrassi.j@gmail.com>
2024-11-30 22:22:23 -0300

    feat(Entity): add canMoveTo method for collision detection before movement

--- Entity.js
+++ Entity.js
@@ -5,6 +5,36 @@ class Entity extends GameObject {
     super(x, y, width, height, color, speed)
   }
 
+  // Check if the entity can move to a certain direction without colliding with
+  // any of the collision objects
+  canMoveTo(direction, collisionObjects) {
+    let x = this.x // Save the x position
+    let y = this.y // Save the y position
+    let canMove = true
+
+    switch (direction) {
+      case Direction.UP:
+        this.y -= this.speed
+        break
+      case Direction.DOWN:
+        this.y += this.speed
+        break
+      case Direction.LEFT:
+        this.x -= this.speed
+        break
+      case Direction.RIGHT:
+        this.x += this.speed
+        break
+    }
+
+    if (this.isCollidingArray(collisionObjects)) canMove = false
+
+    this.x = x // Reset the x position
+    this.y = y // Reset the y position
+
+    return canMove
+  }
+
   move(direction, collisionObjects) {
     let oldY = this.y // Save the old y position
     let oldX = this.x // Save the old x position
