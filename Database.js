112df3e7e35118abdad1c7ec02d9d0807f846233
Andre-Grassi <andregrassi.j@gmail.com>
2024-12-01 15:01:21 -0300

    feat(database): add delete function to Database module for Firestore management

--- Database.js
+++ Database.js
@@ -2,15 +2,28 @@
 import { db } from './firebase-config.js'
 import {
   collection,
+  doc,
   addDoc,
+  deleteDoc,
 } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js'
 
 const Database = {
   post: async function (collectionName, objectData) {
     try {
-      await addDoc(collection(db, collectionName), objectData)
+      const docRef = await addDoc(collection(db, collectionName), objectData)
+      console.log('Document written with ID: ', docRef.id)
+      return docRef.id
     } catch (e) {
       console.error('Error posting: ', e)
+    }
+  },
+
+  delete: async function (collectionName, docId) {
+    try {
+      const docRef = doc(db, collectionName, docId)
+      await deleteDoc(docRef)
+    } catch (e) {
+      console.error('Error deleting: ', e)
     }
   },
 }
