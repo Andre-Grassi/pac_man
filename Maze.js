3588f8a9ad22b693cabf3445942d7cde4b6eedf1
Andre-Grassi <andregrassi.j@gmail.com>
2024-12-03 17:22:39 -0300

    feat(game): restructure wall object creation and resizing logic in Maze class

--- Maze.js
+++ Maze.js
@@ -10,27 +10,51 @@ class Maze {
     this.tileHeight = display.canvas.height / mazeArray.length
 
     // Array of game objects that represent each wall in the maze
-    this.wallObjects = []
+    this.wallObjects = Array.from({ length: mazeArray.length }, () => [])
 
     // Create game objects for each wall
     for (let i = 0; i < this.mazeArray.length; i++)
-      for (let j = 0; j < this.mazeArray[i].length; j++)
-        if (this.mazeArray[i][j] === 1)
-          this.wallObjects.push(
-            new GameObject(
-              this.tileWidth * j,
-              this.tileHeight * i,
-              this.tileWidth,
-              this.tileHeight,
-              tileColor
-            )
+      for (let j = 0; j < this.mazeArray[i].length; j++) {
+        let tile = null
+
+        if (this.mazeArray[i][j] === TileType.WALL)
+          tile = new GameObject(
+            this.tileWidth * j,
+            this.tileHeight * i,
+            this.tileWidth,
+            this.tileHeight,
+            tileColor
           )
+
+        this.wallObjects[i].push(tile)
+      }
+
+    console.log(this.wallObjects)
   }
 
   draw(display) {
-    this.wallObjects.forEach((object) =>
-      object.drawRectangleBorder(display, tileBorderColor, 3)
-    )
+    this.wallObjects.forEach((objectArray) => {
+      objectArray.forEach((object) => {
+        if (object) object.drawRectangleBorder(display, tileBorderColor, 3)
+        //console.log(object.width, object.height)
+      })
+    })
+  }
+
+  resize(display) {
+    this.tileWidth = display.canvas.width / this.mazeArray[0].length
+    this.tileHeight = display.canvas.height / this.mazeArray.length
+
+    for (let i = 0; i < this.wallObjects.length; i++)
+      for (let j = 0; j < this.wallObjects[i].length; j++) {
+        if (this.wallObjects[i][j]) {
+          console.log(this.wallObjects[i].length)
+          this.wallObjects[i][j].width = this.tileWidth
+          this.wallObjects[i][j].height = this.tileHeight
+          this.wallObjects[i][j].x = this.tileWidth * j
+          this.wallObjects[i][j].y = this.tileHeight * i
+        }
+      }
   }
 }
 
