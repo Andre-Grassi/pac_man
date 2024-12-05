a31bce78e96b5d68757f8fa3c871d26a0c10c0e4
Andre-Grassi <andregrassi.j@gmail.com>
2024-12-05 00:19:29 -0300

    refactor(joystick): remove console logs from joystick input handling

--- Joystick.js
+++ Joystick.js
@@ -33,22 +33,18 @@ class Joystick {
       case 37: // Arrow Left
       case 65: // A
         this.left = state
-        console.log('left')
         break
       case 38: // Arrow Up
       case 87: // W
         this.up = state
-        console.log('up')
         break
       case 39: // Arrow Right
       case 68: // D
         this.right = state
-        console.log('right')
         break
       case 40: // Arrow Down
       case 83: // S
         this.down = state
-        console.log('down')
         break
     }
   }
