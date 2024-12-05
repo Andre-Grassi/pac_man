03698249caae43c8803ad151522ca29d10f4519b
Andre-Grassi <andregrassi.j@gmail.com>
2024-12-05 00:35:21 -0300

    feat(firebase): remove outdated comment in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,6 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebas
 
 // Import Firestore (database) from the Firebase SDK
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js'
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Web app's Firebase configuration
 const firebaseConfig = {
